f4c081bf0a1dc43cf8d9883fc38fdd43a2cc018b
Matheus Prado <matheusprador@gmail.com>
2024-01-20 12:34:44 -0300

    adjust: fix autoload for helper functions

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,6 @@ use Adige\core\BaseEnvironment;
 use Adige\core\Adige;
 
 require ROOT . "vendor/larapack/dd/src/helper.php";
-require ROOT . "src/helpers/functions.php";
 
 Adige::commons();
 BaseEnvironment::readEnv(ROOT . '/.env');
